bd2afaa4eba29de627274247b7035a24dcc4d1ad
perkabem-mpw <perkabem@gmail.com>
2026-07-01 11:42:11 +0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -23,9 +23,12 @@ async function startScanner(){
 
                     console.log(result.text);
 
-                    alert("QR : " + result.text);
-
                     codeReader.stop();
+
+                    window.location.replace(
+                    "https://script.google.com/macros/s/AKfycbzk78w5BqDWSPOmCsNJe_QfMwVvqhsFD0HLe4ypCb0zt3SEDbF-RvvZyw1tkrLDWWXolQ/exec?page=scanResult&memberId="
+                    + encodeURIComponent(result.text)
+                            );
 
                 }
 
